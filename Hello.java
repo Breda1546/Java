bca61a28c69ee1882f79376ce20cea0ed5547247
Breda1546 <44686120+Breda1546@users.noreply.github.com>
2019-06-05 19:49:50 -0300

    ADD o arquivo Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/** 
+* Hello World Java 
+* @author Nicolas Breda 
+*/
+
+public class Hello{
+	// metodo principal
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
